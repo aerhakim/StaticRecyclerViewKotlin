13cb3ea4ee1942a0f37ae28189610f96271e1b39
AR Hakim <79903756+aerhakim@users.noreply.github.com>
2021-12-14 16:56:03 +0700

    Mini update

--- app/src/main/java/io/github/aerhakim/androidfundamental/KalkulatorActivity.kt
+++ app/src/main/java/io/github/aerhakim/androidfundamental/KalkulatorActivity.kt
@@ -2,65 +2,67 @@ package io.github.aerhakim.androidfundamental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class KalkulatorActivity : AppCompatActivity() {
 
-    lateinit var angka1: EditText
-    lateinit var angka2: EditText
-    lateinit var kali: Button
-    lateinit var bagi: Button
-    lateinit var kurang: Button
-    lateinit var tambah: Button
-    lateinit var hapus: Button
-    lateinit var hasil: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kalkulator)
-        angka1 = findViewById(R.id.angka1)
-        angka2 = findViewById(R.id.angka2)
-        kali = findViewById(R.id.kali)
-        bagi = findViewById(R.id.bagi)
-        kurang = findViewById(R.id.kurang)
-        tambah = findViewById(R.id.tambah)
-        hasil = findViewById(R.id.hasil)
-        hapus = findViewById(R.id.hapus)
+        val angka1 = findViewById<EditText>(R.id.angka1)
+        val angka2 = findViewById<EditText>(R.id.angka2)
+        val kali = findViewById<Button>(R.id.kali)
+        val bagi = findViewById<Button>(R.id.bagi)
+        val kurang = findViewById<Button>(R.id.kurang)
+        val tambah = findViewById<Button>(R.id.tambah)
+        val hasil = findViewById<TextView>(R.id.hasil)
+        val hapus = findViewById<Button>(R.id.hapus)
+        val jawaban = findViewById<TextView>(R.id.jawaban)
 
-                kali.setOnClickListener {
-                        val angka1 = angka1.text.toString().trim().toFloat()
-                        val angka2 = angka2.text.toString().trim().toFloat()
-                        val hitung = angka1 * angka2
-                        hasil.text = hitung.toString()
-                }
+        kali.setOnClickListener {
+            val angka1 = angka1.text.toString().trim().toFloat()
+            val angka2 = angka2.text.toString().trim().toFloat()
+            val hitung = angka1 * angka2
+            jawaban.visibility = View.VISIBLE
+            hasil.visibility = View.VISIBLE
+            hasil.text = hitung.toString()
+        }
 
-                bagi.setOnClickListener {
-                val angka1 = angka1.text.toString().trim().toFloat()
-                val angka2 = angka2.text.toString().trim().toFloat()
-                val hitung = angka1 / angka2
-                hasil.text = hitung.toString()
-                }
+        bagi.setOnClickListener {
+            val angka1 = angka1.text.toString().trim().toFloat()
+            val angka2 = angka2.text.toString().trim().toFloat()
+            val hitung = angka1 / angka2
+            jawaban.visibility = View.VISIBLE
+            hasil.visibility = View.VISIBLE
+            hasil.text = hitung.toString()
+        }
 
-                kurang.setOnClickListener {
-                val angka1 = angka1.text.toString().trim().toFloat()
-                val angka2 = angka2.text.toString().trim().toFloat()
-                val hitung = angka1 - angka2
-                hasil.text = hitung.toString()
-                }
+        kurang.setOnClickListener {
+            val angka1 = angka1.text.toString().trim().toFloat()
+            val angka2 = angka2.text.toString().trim().toFloat()
+            val hitung = angka1 - angka2
+            jawaban.visibility = View.VISIBLE
+            hasil.visibility = View.VISIBLE
+            hasil.text = hitung.toString()
+        }
 
-                tambah.setOnClickListener {
-                val angka1 = angka1.text.toString().trim().toFloat()
-                val angka2 = angka2.text.toString().trim().toFloat()
-                val hitung = angka1 + angka2
-                hasil.text = hitung.toString()
-                }
+        tambah.setOnClickListener {
+            val angka1 = angka1.text.toString().trim().toFloat()
+            val angka2 = angka2.text.toString().trim().toFloat()
+            val hitung = angka1 + angka2
+            jawaban.visibility = View.VISIBLE
+            hasil.visibility = View.VISIBLE
+            hasil.text = hitung.toString()
+        }
 
-                hapus.setOnClickListener {
-                    angka1.getText().clear();
-                    angka2.getText().clear();
-                }
+        hapus.setOnClickListener {
+            angka1.getText().clear();
+            angka2.getText().clear();
+        }
 
 
     }
